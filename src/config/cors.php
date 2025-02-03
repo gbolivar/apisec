c832f504a19c1788ee23d412a45041280b83e6ea
Gregorio Bolivar <elalconxvii@gmail.com>
2025-02-03 19:00:00 -0300

    bugfix config sonar

--- src/config/cors.php
+++ src/config/cors.php
@@ -18,7 +18,7 @@ return [
     'paths' => ['api/*', 'sanctum/csrf-cookie'],
 
     'allowed_methods' => ['GET', 'POST', 'PUT'],
-    'allowed_origins' => ['https://venezuelahackingteam.com', 'http://localhost'],
+    'allowed_origins' => ['http://vps-4592279-x.dattaweb.com', 'http://localhost'],
     'allowed_origins_patterns' => [],
     'max_age' => 600,
     'supports_credentials' => false,
