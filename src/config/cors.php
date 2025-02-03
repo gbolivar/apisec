ed42a0140748159fd4258ac840e04eb129c7194a
Gregorio Bolivar <elalconxvii@gmail.com>
2025-02-03 14:32:32 -0300

    bugfix ci cd

--- src/config/cors.php
+++ src/config/cors.php
@@ -1,12 +1,34 @@
 <?php
 
 return [
-    'paths'                    => ['api/*', 'sanctum/csrf-cookie'], // Aplica a todas las rutas API y Sanctum si se usa
-    'allowed_methods'          => ['*'], // Permitir todos los métodos (GET, POST, PUT, DELETE, etc.)
-    'allowed_origins'          => ['*'], // Permitir todas las orígenes
-    'allowed_origins_patterns' => [], // Expresiones regulares para orígenes específicos (opcional)
-    'allowed_headers'          => ['*'], // Permitir todos los encabezados
-    'exposed_headers'          => [], // Encabezados adicionales que quieres exponer
-    'max_age'                  => 0, // Tiempo en segundos para almacenamiento en caché de las respuestas preflight
-    'supports_credentials'     => false, // Cambiar a true si necesitas admitir credenciales como cookies
+
+    /*
+    |--------------------------------------------------------------------------
+    | Cross-Origin Resource Sharing (CORS) Configuration
+    |--------------------------------------------------------------------------
+    |
+    | Here you may configure your settings for cross-origin resource sharing
+    | or "CORS". This determines what cross-origin operations may execute
+    | in web browsers. You are free to adjust these settings as needed.
+    |
+    | To learn more: https://developer.mozilla.org/en-US/docs/Web/HTTP/CORS
+    |
+    */
+
+    'paths' => ['api/*', 'sanctum/csrf-cookie'],
+
+    'allowed_methods' => ['*'],
+
+    'allowed_origins' => ['*'],
+
+    'allowed_origins_patterns' => [],
+
+    'allowed_headers' => ['*'],
+
+    'exposed_headers' => [],
+
+    'max_age' => 0,
+
+    'supports_credentials' => false,
+
 ];
